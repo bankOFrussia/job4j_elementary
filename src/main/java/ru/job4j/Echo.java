a4a7398394497f1e252000ebcd16fa8f7af33983
bankOFrussia <carlsbergrussia@gmail.com>
2022-09-18 22:20:26 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,19 +1,5 @@
 package ru.job4j;
 
 public class Echo {
-    byte age = 30;
 
-    char sex = 'M';
-
-    float height = 1.8f;
-
-    int brick = 40000;
-
-    long bacterium = 10_000_000_000L;
-
-    int sun = 149_600_000;
-
-    boolean workPermission = true;
-
-    long file = 2_684_354_560L;
 }
