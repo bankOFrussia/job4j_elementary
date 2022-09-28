5921965c6459ecdd012c329b7f85286a655f2c0c
bankOFrussia <carlsbergrussia@gmail.com>
2022-09-28 20:42:09 +0300

    5.0.1. Сумма чисел. [#226416 #346723] 1

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,7 +3,8 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int i = start; i <= finish; i++) {
+        int i;
+        for (i = start; i <= finish; i++) {
             sum = sum + i;
         }
         return sum;
