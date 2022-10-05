f42c9466630d5ae90246faa31d541a28bb8bdbc3
bankOFrussia <carlsbergrussia@gmail.com>
2022-10-05 22:33:11 +0300

    6.9. String - это массив [#504884 #348493]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -5,13 +5,15 @@ import static java.lang.Character.isDigit;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean rsl = false;
         char[] array = name.toCharArray();
-
-        if (!(name.isEmpty() || isDigit(name.charAt(0)) || isUpperLatinLetter(array[0]))) {
+        boolean rsl = !(name.isEmpty() || isDigit(name.charAt(0)) || isUpperLatinLetter(array[0]));
+        if (rsl) {
             for (int i = 1; i < name.length(); i++) {
                 int code = name.codePointAt(i);
                 rsl = isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code) || isDigit(code);
+                if (!rsl) {
+                    break;
+                }
             }
         }
         return rsl;
