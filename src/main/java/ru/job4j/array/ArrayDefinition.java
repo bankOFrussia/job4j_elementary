26501bdbb21b4bd7ee1cfbc35f53af91d7cc9625
bankOFrussia <carlsbergrussia@gmail.com>
2022-10-08 23:40:32 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,18 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         String[] names = new String[4];
         names[0] = "Ivan";
         names[1] = "Petr";
         names[2] = "Serg";
         names[3] = "Max";
-
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
         System.out.println(names[3]);
-
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -15,13 +15,11 @@ public class Converter {
         float dollar = Converter.rubleToDollar(120);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("120 rubles are " + dollar + " dollars");
-
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
-
         float in2 = 120;
         float expected2 = 2;
         float out2 = Converter.rubleToDollar(in2);
