46187e21ae3b58b7dee44b22da0f17476953d194
bankOFrussia <carlsbergrussia@gmail.com>
2022-11-06 16:13:40 +0300

    from tracker

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+        System.out.println("Поехали.");
+    }
+
+    @Override
+    public void passengers(int number) {
+        System.out.println("Загрузили пассажиров.");
+    }
+
+    @Override
+    public double getFuel(double fuelAmount) {
+        System.out.println("Заправили топливо.");
+        return 0;
+    }
+}
