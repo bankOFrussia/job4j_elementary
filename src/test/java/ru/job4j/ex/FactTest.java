b41639c227a57bdca4b22ab59b2c0edfa2852015
bankOFrussia <carlsbergrussia@gmail.com>
2022-11-06 17:02:22 +0300

    Тестирование исключений с junit

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -12,7 +12,7 @@ public class FactTest {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> Fact.calc(-1));
-        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
+        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0.");
     }
 
     @Test
@@ -20,7 +20,7 @@ public class FactTest {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> Fact.calc(-22));
-        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
+        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0.");
     }
 
     @Test
