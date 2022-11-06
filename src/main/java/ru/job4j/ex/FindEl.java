2fa4af70f0b4b70ce434a974c591c2955e8d4bf7
bankOFrussia <carlsbergrussia@gmail.com>
2022-11-06 16:44:55 +0300

    Пользовательские исключения

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,10 +6,11 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
-            throw new ElementNotFoundException();
+            throw new ElementNotFoundException("Элемент не найден");
         }
         return rsl;
     }
