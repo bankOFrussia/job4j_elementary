39c9fa834690b909942b53c45a70105e21109a3d
bankOFrussia <carlsbergrussia@gmail.com>
2022-11-07 23:16:29 +0300

    Иерархия исключений и множественный catch

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,38 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        boolean on = true;
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                on = false;
+                return user;
+            }
+            if (on) {
+                throw new UserNotFoundException("Пользователь не найден");
+            }
+        }
+        return null;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("Пользователь не валидный");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {new User("Petr Arsentev", true)};
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException exception) {
+            exception.printStackTrace();
+        } catch (UserNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
