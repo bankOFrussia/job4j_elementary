dd7169c6adb2ff347131be20e1464c8c9a31a678
bankOFrussia <carlsbergrussia@gmail.com>
2022-11-13 19:39:42 +0300

    Принципы раннего возврата и охранных выражений

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,43 @@
+package ru.job4j.early;
+
+import static java.lang.Character.*;
+
+public class PasswordValidator {
+    public static String validate(String password) throws IllegalArgumentException {
+        int countNumbers = 0;
+        int countSymbols = 0;
+        String[] illegalPasswords = {"qwerty", "password", "admin", "user", "12345"};
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password can't be null");
+        }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password should be length [8, 32]");
+        }
+        if (password.equals(password.toUpperCase())) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (password.equals(password.toLowerCase())) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        for (char one : password.toCharArray()) {
+            if (isDigit(one)) {
+                countNumbers++;
+            } else if (!isLetterOrDigit(one)) {
+                countSymbols++;
+            }
+        }
+        if (countNumbers == 0) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (countSymbols == 0) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
+        for (String pass : illegalPasswords) {
+            if (password.toLowerCase().indexOf(pass) > 0) {
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
+        }
+        return password;
+    }
+}
+
